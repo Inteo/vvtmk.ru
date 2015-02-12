2d2908a82e0c69844e8f3e3f8f4d3ab7839b64ef
BerTepesh <Ber.Tepesh@gmail.com>
2015-02-12 14:30:37 +0300

    Правки по вентиляции

--- f/js/common.js
+++ f/js/common.js
@@ -13,3 +13,12 @@ $(function(){
 		$(this).parent().parent().find(".b-tabs__content").eq($(".b-tabs__btn").index(this)).addClass("b-tabs__content_active");
 	});
 });
+$(function(){
+  $(".b-scroll-btn").click(function(){
+    var theId = $(this).attr("id");
+    var theIds = theId.match(/\w+|"[^"]+"/g);
+    var str = theIds.join(' ');
+    var num = parseInt(str.replace(/\D+/g, ""));
+    $.scrollTo($("#scroll-to-"+num), 350);
+  });
+});
