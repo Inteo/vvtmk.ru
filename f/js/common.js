41ef9f26dff5dbb7c557f7039c289ffbd9d45336
BerTepesh <Ber.Tepesh@gmail.com>
2015-02-13 14:33:41 +0300

    Гражданская оборона

--- f/js/common.js
+++ f/js/common.js
@@ -1,8 +1,18 @@
-﻿$(document).ready(function(){
+﻿var scrollTargets = {};
+$(document).ready(function(){
 	$(".b-tabs__btn").removeClass("b-tabs__btn_active");
 	$(".b-tabs__btn:first").addClass("b-tabs__btn_active");
 	$(".b-tabs__content").removeClass("b-tabs__content_active");
 	$(".b-tabs__content:first").addClass("b-tabs__content_active");
+	$(".b-ham-menu").find(".icon-ham").removeClass("icon-ham_close");
+	$(".b-ham-menu").removeClass("b-ham-menu_active");
+
+	$('.js-scroll-link').each(function() {
+    var setlink = $(this),
+        settarget = setlink.data('scrollto'),
+        settargetitem = $('.js-scroll-target[data-scrollto="' + settarget + '"]').not(setlink)
+    		scrollTargets[settarget] = settargetitem.offset().top;
+	});
 });
 $(function(){
 	$(".b-tabs__btn").click(function(){
@@ -14,19 +24,40 @@ $(function(){
 	});
 });
 $(function(){
-  $(".b-scroll-btn").click(function(){
-    var theId = $(this).attr("id");
-    var theIds = theId.match(/\w+|"[^"]+"/g);
-    var str = theIds.join(' ');
-    var num = parseInt(str.replace(/\D+/g, ""));
-    $.scrollTo($("#scroll-to-"+num), 350, {offset: -50});
+  $(".js-scroll-link").click(function(){
+  	if(!$(this).hasClass("active")) {
+	    var scrollTo = $(this).data("scrollto"),
+	    scrollTarget = $('*[data-scrollto="'+scrollTo+'"].js-scroll-target');
+	    $.scrollTo(scrollTarget, 350, {offset: -50});
+  	}
   });
   $(".b-what-is__btn").click(function(){
   	$(this).closest(".b-what-is").find(".b-what-is__content").toggleClass("b-what-is__content_active")
   });
+  $(".b-ham-menu__btn").click(function(){
+  	$(this).find(".icon-ham").toggleClass("icon-ham_close");
+  	$(this).closest(".b-ham-menu").toggleClass("b-ham-menu_active");
+  });
+  /*$(".b-ham-menu__item span").click(function(){
+  	if(!$(this).hasClass("active")) {
+  		$(".b-ham-menu__item span").removeClass("active");
+  		$(this).addClass("active");
+  	}
+  });*/
 });
 $.preloadImages = function() {
   for (var i = 0; i < arguments.length; i++) {
     $("<img />").attr("src", arguments[i]);
   }
 }
+
+$(window).on('scroll', function() {
+		var initial = $(document).scrollTop(),
+    anchor = '',
+    margin = 70;
+
+		$.each(scrollTargets, function(JIndex, JElement) {
+		        initial >= JElement - margin && (anchor = JIndex);
+		    }),
+    		$('.js-scroll-link').removeClass('active').filter('[data-scrollto="' + anchor + '"]').addClass('active');
+}).trigger('scroll');
